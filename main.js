63decc439a0c59e9a95efe77ada1bba332aa482c
Morton Fox <github@qslw.com>
2016-10-12 12:06:52 -0400

    Add button action to click all the coupons.

--- main.js
+++ main.js
@@ -1,33 +1,13 @@
-/*
-http://coupons.shoprite.com/main.html
-
-
-<button class="load2crd">Load to Card</button>
-
-id: AllCoupons0
-class: rightDiv
-
-<iframe id="cpsite" src="http://coupons.shoprite.com/LoggedInPage.html?d1=47015120226&amp;d2=cheah1@hotmail.com&amp;d3=c61d4381-53ac-4998-8dca-deb171fc597b"></iframe>
-*/
-
-/*
 function runSelect(event) {
   event.preventDefault();
 
-  var tbody = document.getElementsByTagName('tbody');
-  var rows = tbody[0].getElementsByTagName('tr');
-
-  for (var row of rows) {
-    // Find all table rows with an archived cache.
-    var oldwarning = row.getElementsByClassName('OldWarning');
-    if (oldwarning.length > 0) {
-      // Check the row's checkbox so user can do a bulk delete later.
-      var checkboxes = row.getElementsByClassName('Checkbox');
-      checkboxes[0].checked = true;
-    }
+  var cpsite = document.getElementById('cpsite').contentDocument;
+  var allcouponitems = cpsite.getElementById('all-coupon-items0');
+  var load2crd = allcouponitems.getElementsByClassName('load2crd');
+  for (var btn of load2crd) {
+    btn.click();
   }
 }
-*/
 
 function insertButton(btn) {
   function waitForSite() {
@@ -50,11 +30,9 @@ function init() {
   newbutton.setAttribute('id', 'load_all_to_card');
   newbutton.setAttribute('style', 'background-color: #E82A24; color: #fff; font-weight: 700; border: none; padding: 6px 10px; cursor: pointer;');
   newbutton.appendChild(document.createTextNode('Load All To Card'));
+  newbutton.addEventListener('click', runSelect);
 
   insertButton(newbutton);
-  /*
-  newbutton.addEventListener('click', runSelect);
-  */
 }
 
 init();
